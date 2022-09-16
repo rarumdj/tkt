489b64d29d1e4b16a922a6f30f06b8389ee41ed9
David Johnson <davemoses3@gmail.com>
2022-09-16 10:42:55 +0100

    update

--- src/pages/Dashbaord.jsx
+++ src/pages/Dashbaord.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ItemList from "../components/ItemList";
 import { useDispatch, useSelector } from "react-redux";
 import { getBusinessAction } from "../utils/redux-saga/actions/businessActions";
@@ -32,6 +32,8 @@ const lists = [
 const Dashbaord = () => {
   const dispatch = useDispatch();
 
+  const [filter, setFilter] = useState({ sector: "", company: "" });
+
   const {
     getBusiness: { loading, data },
   } = useSelector(({ business }) => business);
@@ -39,13 +41,20 @@ const Dashbaord = () => {
   useEffect(() => {
     dispatch(getBusinessAction());
   }, [dispatch]);
+
+  const filteredData = data?.filter(
+    (val) =>
+      val.name.includes(filter.company) && val.sector.includes(filter.sector)
+  );
+
   return (
     <div className="w-full">
       <div className="grid md:grid-cols-2 gap-6 md:w-8/12">
         <select
+          onChange={(e) => setFilter({ ...filter, sector: e.target.value })}
           id="countries"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <option selected>Sector *</option>
+          <option value='' selected>Sector *</option>
           {data?.map((item, i) => (
             <option key={i} value={item.sector}>
               {item.sector}
@@ -55,8 +64,9 @@ const Dashbaord = () => {
 
         <select
           id="countries"
+          onChange={(e) => setFilter({ ...filter, company: e.target.value })}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <option selected>Company *</option>
+          <option value='' selected>Company *</option>
           {data?.map((item, i) => (
             <option key={i} value={item.name}>
               {item.name}
@@ -69,7 +79,7 @@ const Dashbaord = () => {
           <div className="spinner-3 m-auto"></div>
         </div>
       )}
-      {data && !loading && <ItemList header={header} lists={data} />}
+      {data && !loading && <ItemList header={header} lists={filteredData} />}
     </div>
   );
 };
