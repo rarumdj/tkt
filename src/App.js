0f26de9be3c649bd9f8b50fb33617e257a7d7d60
David Johnson <davemoses3@gmail.com>
2022-09-16 01:27:35 +0100

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ function App() {
         <DashboardNavbar />
         <section className="relative py-8 lg:pl-8 sm:pl-4 md:pl-0 md-pr-0 pl-2 lg:pr-8 sm:pr-4 pr-2 max-w-[110rem] mx-auto">
           <Route path="/" component={Dashbaord} exact={true} />
-          <Route path="/business-details/:id" component={DetailsPage} exact={true} />
+          <Route
+            path="/business-details/:id"
+            component={DetailsPage}
+            exact={true}
+          />
         </section>
       </main>
     </div>
