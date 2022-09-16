4bf2a31aef040947b8202f28a28cad88deb972c6
David Johnson <davemoses3@gmail.com>
2022-09-16 01:29:30 +0100

    update

--- src/App.js
+++ src/App.js
@@ -14,12 +14,8 @@ function App() {
       <main className="flex-1 h-screen overflow-y-scroll">
         <DashboardNavbar />
         <section className="relative py-8 lg:pl-8 sm:pl-4 md:pl-0 md-pr-0 pl-2 lg:pr-8 sm:pr-4 pr-2 max-w-[110rem] mx-auto">
-          <Route path="/" component={Dashbaord} exact={true} />
-          <Route
-            path="/business-details/:id"
-            component={DetailsPage}
-            exact={true}
-          />
+          <Route path="/" component={Dashbaord} exact />
+          <Route path="/business-details/:id" component={DetailsPage} exact />
         </section>
       </main>
     </div>
